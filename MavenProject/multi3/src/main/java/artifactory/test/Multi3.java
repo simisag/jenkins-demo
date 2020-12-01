60fd259ab019e3f41a6eb204b78876b2111d4f65
simisag <40498998+simisag@users.noreply.github.com>
2020-12-01 20:58:52 +0530

    Update Multi3.java

--- MavenProject/multi3/src/main/java/artifactory/test/Multi3.java
+++ MavenProject/multi3/src/main/java/artifactory/test/Multi3.java
@@ -7,5 +7,6 @@ public class Multi3 {
     public static void main(String[] args) {
         new Multi1();
         System.out.println("Hello World!");
+        System.out.println("Hello World Again!");
     }
 }
